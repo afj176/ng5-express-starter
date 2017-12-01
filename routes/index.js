a5c1fc7908e8eebf73117a0e28bfd08e534920f5
Antonio Fernandes <afj176@gmail.com>
2017-11-30 21:11:52 -0500

    upgrade example to angular 5 and simplified

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html', { title: 'ng2 Express Starter' });
+  res.render('index.html', { title: 'Angular 5 / Express Starter' });
 });
 
 module.exports = router;
